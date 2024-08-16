c2463ec4a2af62e313562284836f905bd1f63150
cuongyb099 <133899294+cuongyb099@users.noreply.github.com>
2024-08-16 09:58:30 +0700

    save change

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+using ResilientCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody Rigidbody { get; private set; }
+    
+    
+    public float Speed = 5.0f;
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+ 
+    void FixedUpdate()
+    {
+        Rigidbody.velocity = new Vector3(0f,Rigidbody.velocity.y,0f);
+        Rigidbody.AddForce(PlayerInput.Instance.MovementInput* Speed,ForceMode.VelocityChange);
+        RotatePlayer();
+    }
+    public void RotatePlayer()
+    {
+        Vector2 rotateInput = PlayerInput.Instance.RotationInput;
+        if (rotateInput == Vector2.zero) return;
+        float atan = Mathf.Atan2(rotateInput.x,rotateInput.y)*Mathf.Rad2Deg;
+        Rigidbody.rotation = Quaternion.Euler(0,atan,0);
+    }
+}
