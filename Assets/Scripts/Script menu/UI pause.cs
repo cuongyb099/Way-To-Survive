47387a95bcbc2998d026a2dd3df4147da71d3669
HoangNgocDuy <hoangngocduy23082003@gmail.com>
2024-10-31 15:44:56 +0700

    update UI pause

---
+++ Assets/Scripts/Script menu/UI pause.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public Button resumeButton;
+    public Button settingsButton;
+    public Button quitButton;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        resumeButton.onClick.AddListener(Resume);
+        settingsButton.onClick.AddListener(OpenSettings);
+        quitButton.onClick.AddListener(QuitGame);
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Dừng thời gian game
+        isPaused = true;
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Tiếp tục thời gian game
+        isPaused = false;
+    }
+
+    void OpenSettings()
+    {
+        // Logic để mở cài đặt
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
