ee30b47edd206f07798da8b7b915cdd349a07a5e
Tuan18ND <ngotuan24122003@gmail.com>
2024-11-03 21:45:56 +0700

    AnimationControl

---
+++ Assets/Animation/AnimationControl/AnimationControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        // Kiểm tra trạng thái di chuyển để kích hoạt animation đi bộ
+        bool isWalking = Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0;
+        animator.SetBool("isWalking", isWalking);
+
+        // Animation khi bắn súng
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("isShooting", true);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("isShooting", false);
+        }
+    }
+}
