e5bb63ac638d60e094ab992e0c7482e73b6c0e9e
Tuan18ND <ngotuan24122003@gmail.com>
2024-11-22 16:53:15 +0700

    Update AnimationzombieControl.cs

--- Assets/Animation/AnimationControl/AnimationzombieControl.cs
+++ Assets/Animation/AnimationControl/AnimationzombieControl.cs
@@ -8,29 +8,34 @@ public class ZombieAI : MonoBehaviour
     public float moveSpeed = 3.5f;
     private NavMeshAgent agent;
 
-    void Start()
+    private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed; // Đặt tốc độ di chuyển của zombie
     }
 
-    void Update()
+    private void Update()
     {
         float distance = Vector3.Distance(player.position, transform.position);
         if (distance < attackRange)
         {
-            // Tấn công người chơi
             Attack();
         }
         else
         {
-            // Di chuyển về phía người chơi
-            agent.SetDestination(player.position);
+            MoveTowardsPlayer();
         }
     }
 
-    void Attack()
+    private void MoveTowardsPlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Attack()
     {
         // Logic tấn công (như mất máu)
         Debug.Log("Zombie attacks!");
+        // Bạn có thể thêm logic để gây sát thương cho người chơi tại đây.
     }
 }
